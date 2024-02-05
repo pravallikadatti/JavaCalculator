74141be01521d3388b7e087d3baf2fca85045a9f
anilkumar3577 <107928396+anilkumar3577@users.noreply.github.com>
2024-02-05 12:53:05 +0530

    Update and rename code.groovy to mavenjarpipeline.groovy

--- vars/code.groovy
+++
@@ -1,4 +0,0 @@
-def call() {
-   // do some stuff
-   return 'hello'
-}
---
+++ vars/mavenjarpipeline.groovy
@@ -0,0 +1,41 @@
+pipeline {
+    agent any
+    tools {
+        git 'Default'
+        dockerTool 'docker'
+        maven 'mvn'
+    }
+    stages{
+        stage(code){
+            steps {
+               git 'https://github.com/anilkumar3577/JavaCalculator.git' 
+            }
+        }
+        stage(package){
+            steps {
+               sh 'mvn package'
+            }
+        }
+        stage(test){
+            steps {
+             sh 'mvn test'
+            }
+        }
+        stage(build){
+            steps {
+              sh 'docker build -t share .' 
+            }
+        }
+        stage(push){
+            steps {
+            withCredentials([usernamePassword(credentialsId: 'dock', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
+ sh ''' docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
+ docker tag share ${DOCKER_USERNAME}/share
+        docker push ${DOCKER_USERNAME}/share '''
+             }
+
+            }
+        }
+    }
+}
+
